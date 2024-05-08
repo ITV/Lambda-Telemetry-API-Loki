fa096351a10d02d3cbe880c0b872438efbf9b384
Tom Haynes <tom.haynes@itv.com>
2024-05-08 10:44:43 +0100

    itv-stuff

--- loki_extension/loki/promtail.go
+++ loki_extension/loki/promtail.go
@@ -9,50 +9,48 @@ import (
 )
 
 type LogEntry struct {
-	Record string    `json:"record"`
+	Record string `json:"record"`
 	Time   string `json:"time"`
-	Type   string    `json:"type"`
+	Type   string `json:"type"`
 }
 
 var (
-	loki promtail.Client
-	conf promtail.ClientConfig
+	loki          promtail.Client
+	conf          promtail.ClientConfig
 	function_name string
-	err error
+	err           error
 )
+
 // Init Promtail Client
-func init(){
+func init() {
 	source_name := "Lambda"
 	function_name = os.Getenv("AWS_LAMBDA_FUNCTION_NAME")
-	labels := "{source=\""+source_name+"\",function=\""+function_name+"\"}"
+	labels := "{source=\"" + source_name + "\",function=\"" + function_name + "\"}"
 	lokiIp := os.Getenv("LOKI_IP")
 	if len(lokiIp) == 0 {
 		panic("LOKI Ip undefined")
 	}
 	conf = promtail.ClientConfig{
-		PushURL:            "http://"+lokiIp+":3100/api/prom/push",
+		PushURL:            lokiIp + "/api/prom/push",
 		Labels:             labels,
 		BatchWait:          5 * time.Second,
-		BatchEntriesNumber: 10000,
-		SendLevel: 			promtail.INFO,
-		PrintLevel: 		promtail.ERROR,
+		BatchEntriesNumber: 1,
+		SendLevel:          promtail.INFO,
+		PrintLevel:         promtail.DEBUG,
 	}
 	loki, err = promtail.NewClientProto(conf)
 	if err != nil {
 		log.Println("Promtail init error")
 		log.Println(err)
 	}
-	
-
-
 }
 
-func LokiSend(record *string){
+func LokiSend(record *string) {
 	tstamp := time.Now().String()
 	source_name := "lambda"
 	loki.Infof("source = %s, function= %s,  time = %s, record = %v\n", source_name, function_name, tstamp, *record)
 }
 
-func LokiShutdown(){
+func LokiShutdown() {
 	loki.Shutdown()
 }
